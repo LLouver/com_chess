948ff174e365381570f1127431ff1caa17f26bc5
Jarvis <1351293038@qq.com>
2022-10-12 19:54:21 +0800

    test nmsl commit

--- src/js/chessGame.js
+++ src/js/chessGame.js
@@ -1,8 +1,11 @@
 //chessGame.js
 //内部操作
+//NMSL
 "use strict";
 //选定了一个棋子，标出他能走到的格子和能攻击的格子
-function markValid(situation,board,piece){}
+function markValid(situation,board,piece){
+
+}
 
 //标记出被某方棋子攻击的所有格子，包括被国王攻击
 /*调用markValid*/
@@ -14,11 +17,15 @@ function invalidMove(situation,board,piece,x,y){}
 
 //检查某方是否无路可走
 /*调用markAttack*/
-function isEven(situation,side){}
+function isEven(situation,side){
+
+}
 
 //检查某方是否将军（A将军，B被将军
 /*调用markAttack*/
-function isCheck(situation,side){}
+function isCheck(situation,side){
+
+}
 
 //（已知某方将军）检查某方是否将对方将杀（检查B能否应将
 /*调用isCheck*/
